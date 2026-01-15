cda8728f6263b7ac46d259f174aadbc380dd469f
agilityb <nwakwuruibebright@gmail.com>
2026-01-16 00:18:19 +0100

    W01 Assignment - Homepage

---
+++ scripts/getdates.js
@@ -0,0 +1,9 @@
+
+
+    
+const yearSpan = document.getElementById("currentyear");
+
+const lastModified = document.getElementById("lastModified");
+
+yearSpan.textContent = new Date().getFullYear();
+lastModified.textContent = `Last Modified: ${document.lastModified}`;
