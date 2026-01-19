0b604f8e657fa3d8a47c9c665fee9a238320c8da
agilityb <nwakwuruibebright@gmail.com>
2026-01-19 19:09:14 +0100

    W02 Assignments -  DOM

---
+++ scripts/temples.js
@@ -0,0 +1,10 @@
+
+
+
+const yearSpan = document.getElementById("currentyear");
+
+const lastModified = document.getElementById("lastModified");
+
+
+yearSpan.textContent = new Date().getFullYear();
+lastModified.textContent = `Last Modified: ${document.lastModified}`;
---
+++ week02/scripts/bom.js
@@ -0,0 +1,33 @@
+
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#lists'); 
+
+
+//Event Listener for Add chapter button
+
+button.addEventListener("click", () => {
+    if (input.value.trim() != "") {
+
+        const li = document.createElement("li");
+        const deleteButton = document.createElement("button");
+
+        li.textContent = input.value;
+        deleteButton.textContent = "Remove Chapter❌";
+
+        li.append(deleteButton);
+        list.appendChild(li);
+
+        input.value = "";
+        input.focus();
+
+        //event listener for delete button
+        deleteButton.addEventListener("click", () => {
+            list.removeChild(li);
+            input.focus();
+        });
+    }
+    else {
+        alert("Please Enter a chapter you wished to add");
+    }
+});
